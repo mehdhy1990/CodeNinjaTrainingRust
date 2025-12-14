53742da78dbcba806314c3695d2a2859ca73d531
Mehdi Heidari <hymehdi84@gmail.com>
2025-12-14 21:56:46 +0100

    playing banjo in done

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,9 @@
-mod rock_paper_scissors;
-mod rental_car_cost;
-mod find_difference;
+mod are_you_playing_banjo;
 
 fn main() {
 
 
-   let output =find_difference::find_difference(&[3, 2, 5], &[1, 4, 4]);
-    println!("{}", output);
+
 }
 
 
