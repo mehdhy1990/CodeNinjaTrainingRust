06decac4680042cc4eed258fd90c74ddf4ca6a70
Mehdi Heidari <hymehdi84@gmail.com>
2025-12-15 14:01:26 +0100

    solved open or senior

--- src/main.rs
+++ src/main.rs
@@ -3,10 +3,11 @@ mod double_char;
 mod positive_sum;
 mod abbrev_name;
 mod grow;
+mod open_or_senior;
 
 fn main() {
-let vec = vec![1, 2, 3, 4];
-    let result = grow::grow(vec);
-    println!("{}", result);
+let vec:Vec<(i32,i32)> = vec![(18, 20), (45, 2), (61, 12), (37, 6), (21, 21), (78, 9)];
+    let result =open_or_senior::open_or_senior(vec);
+    println!("{:#?}", result);
 }
 
