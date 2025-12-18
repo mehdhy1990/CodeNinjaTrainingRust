05cfa5ab82c39139b8e446086420dfd0af0d13c4
Mehdi Heidari <hymehdi84@gmail.com>
2025-12-18 23:15:06 +0100

    phone number is done

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,12 @@
-mod get_count;
+
 
 fn main() {
-    let result = get_count::get_count("hello world");
+    let result = create_phone_number(&[1,2,3,4,5,6,7,8,9,0]);
     println!("{}", result);
 }
 
+fn create_phone_number(numbers: &[u8]) -> String {
+  let strings=  numbers.iter().map(|x| x.to_string()).collect::<String>();
+    format!("({}) {}-{}",&strings[..3],&strings[3..6],&strings[6..])
+
+}
