cdf2438971810121c1c735480728d142f873b2e7
Mehdi Heidari <hymehdi84@gmail.com>
2025-12-19 21:23:10 +0100

    done find the missing letter

--- src/rank_up_files.rs
+++ src/rank_up_files.rs
@@ -47,3 +47,17 @@ pub fn square_digits(num: u64) -> u64 {
     let result=   digits.iter().rev().map(|d| (d*d).to_string()).collect::<Vec<_>>().join("").parse::<u64>();
     result.unwrap()
 }
+pub fn find_missing_letter(chars: &[char]) -> char {
+    let vec = chars.iter().map(|&x| x as u8).collect::<Vec<u8>>();
+    let result = vec.windows(2).find(|x| x[0] + 1 != x[1]).unwrap();
+    (result[0] + 1) as char
+
+
+    //best practice
+    // -----------------------------------------
+    // (chars.windows(2)                        //Convert into a list of arrays length 2, for ease of viewing past and next character.
+    //     .map(|w| (w[0] as u8, w[1] as u8)) //Convert the character array to a byte tuple.
+    //     .find(|&w| w.0 + 1 != w.1)         //Example: 'a' + 1 != 'b'? Found a missing character.
+    //     .unwrap().0 + 1) as char           //Add 1 to previous character, to get the correct character.
+
+}
