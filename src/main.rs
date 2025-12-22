5179942dac79c0ec58dab143a97f6f58d4a840cc
Mehdi Heidari <hymehdi84@gmail.com>
2025-12-22 16:52:41 +0100

    rot 13 is done

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,3 @@
-
 mod rank_up_files;
 
 fn main() {
@@ -14,3 +13,15 @@ fn main() {
 //     }
 //     todo!()
 // }
+fn rot13(message: &str) -> String {
+    message
+        .chars()
+        .map(|c| {
+            match c {
+                'a'..='m' | 'A'..='M' => ((c as u8) + 13) as char,
+                'n'..='z' | 'N'..='Z' => ((c as u8) - 13) as char,
+                _ => c, // Keep numbers, spaces, and symbols as they are
+            }
+        })
+        .collect()
+}
